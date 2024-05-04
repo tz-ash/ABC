186675f37849299fa210818bb741652eb3d0e8ba
Kid_of_devil <82517468+tz-ash@users.noreply.github.com>
2024-05-04 15:58:24 +0530

    Update wall.py

--- AnonXMusic/plugins/tools/wall.py
+++ AnonXMusic/plugins/tools/wall.py
@@ -41,7 +41,7 @@ def pic_command(client, message: Message):
     try:
         response = requests.get(unsplash_url)
         if response.status_code == 200:
-            client.send_photo(message.chat.id, photo=unsplash_url, caption=f"✦ ʜᴇʀᴇ's ᴀ ᴘɪᴄᴛᴜʀᴇ ʀᴇʟᴀᴛᴇᴅ ᴛᴏ ➠ {name}.\n\n✦ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➥ ๛ᴀ ᴠ ɪ s ʜ ᴀ ࿐" )
+            client.send_photo(message.chat.id, photo=unsplash_url, caption=f"✦ ʜᴇʀᴇ's ᴀ ᴘɪᴄᴛᴜʀᴇ ʀᴇʟᴀᴛᴇᴅ ᴛᴏ ➠ {name}.\n\n✦ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➥ ๛ᴍ ɪ ʟ ᴏ ࿐" )
         else:
             client.send_message(message.chat.id, "✦ Failed to fetch image.")
     except requests.RequestException as e:
